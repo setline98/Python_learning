e76c79847c358a7733102f125856977765b8e387
setline98 <setline123s@gmail.com>
2023-09-13 00:19:39 +0400

    Some fixes

--- Task_3.py
+++ Task_3.py
@@ -1,5 +1,5 @@
 def main():
-    number1 = int(input("Введите пераое число: "))
+    number1 = int(input("Введите первое число: "))
     number2 = int(input("Введите второе число: "))
     if number1>number2:
         print("invalid input")
