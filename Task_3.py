6726c2aee4ba90a2fa60d792e82f93f9063591ff
setline98 <setline123s@gmail.com>
2023-09-12 23:09:34 +0400

    First commit

---
+++ Task_3.py
@@ -0,0 +1,16 @@
+def main():
+    number1 = int(input("Введите пераое число: "))
+    number2 = int(input("Введите второе число: "))
+    if number1>number2:
+        print("invalid input")
+        return
+    print("Между " +
+          str(number1) +
+          " и " +
+          str(number2) +
+          " "+
+          str(number2-number1-1)+
+          " места")
+
+if __name__ == '__main__':
+    main()
